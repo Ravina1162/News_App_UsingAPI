f4ef8e803518083aa3d0ce8a0f4472cf4304479a
Ravina1162 <kravin45@gmail.com>
2023-06-19 10:10:07 +0530

    reinitialized files

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
+
+function App() {
+  
+  return (
+    <>
+      <Router>
+      {/* Other components */}
+      <Routes>
+      <Route path="/" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
+      
+    </>
+  );
+}
+
+export default App;
+
+
+
+// return (
+//   <>
+//     {/* <Navbar links={linksArray} /> */}
+//     {/* <Articles/> */}
+//     {/* <Signup/> */}
+//     {/* <Login /> */}
+//     <Router>
+//     {/* Other components */}
+//     <Routes>
+//     <Route path="/" element={<Signup />} />
+//       <Route path="/Login" element={<Login />} />
+//       <Route path="/Home" element={<Home />} />
+//       {/* Other routes */}
+//     </Routes>
+//   </Router>
+    
+//   </>
+// );
+// }
+
+// export default App;
